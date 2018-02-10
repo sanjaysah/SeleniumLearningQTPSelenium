9a406f3f94a1556c4c068a0260e16c0343037ae8
sanjaysah <sanjaysah.engg@gmail.com>
2018-02-10 21:12:30 +0530

    first commit

---
+++ src/main/java/com/qtpselenium/selenium/SeleniumLearningQTPSelenium/App.java
@@ -0,0 +1,13 @@
+package com.qtpselenium.selenium.SeleniumLearningQTPSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
